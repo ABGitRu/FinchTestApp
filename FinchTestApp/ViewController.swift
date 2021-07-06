8788c2a100895af140560531f3a714586e715957
Alexey Barinov <begoodbebad2012@gmail.com>
2021-07-06 18:39:04 +0300

    Initial Commit

---
+++ FinchTestApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FinchTestApp
+//
+//  Created by Mac on 06.07.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
